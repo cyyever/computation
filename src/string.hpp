aecf4cd89606219c17aca971c2dd5b86aad2013d
cyyever <cyyever@outlook.com>
2018-03-11 10:09:37 +0800

    add template

--- src/string.hpp
+++ src/string.hpp
@@ -12,9 +12,10 @@
 namespace cyy::lang {
 
   // find word from str
-  inline symbol_string_view  KMP(symbol_string_view word,symbol_string_view str) {
+ template< class CharT>
+   CharT * KMP(std::basic_string_view<CharT> word,std::basic_string_view<CharT> str) {
     if(word.empty()|| word.size()>str.size()) {
-      return {};
+      return nullptr;
     }
 
     //for each substring w1...ws,compute the longest proper prefix w1...wf(s) that is a suffix of w1...ws
@@ -22,7 +23,7 @@ namespace cyy::lang {
     //f[1] is always empty string,so we begin with w2;
     for(size_t i=1;i<word.size();i++) {
       auto t=failure_function[i-1];
-      while(t!=0 && word[t] !=word[i]) 
+      while(t>0 && word[t] !=word[i]) 
       {
 	t=failure_function[t];
       }
@@ -31,19 +32,20 @@ namespace cyy::lang {
       }
     }
 
-    size_t i=0;
-    while(!str.empty()) {
-      if(word[i]==str[i]) {
-	i++;
-	if(i==word.size()) {
-	  return {str.data(),word.size()};
+    size_t s=0;
+    for(size_t i=0;i<str.size();i++) {
+      auto c=str[i];
+      while(s>0 && word[s]!=c) {
+	s=failure_function[s];
+      }
+      if(word[s]==c) {
+	s++;
+	if(s==word.size()) {
+	  return str.data()+i-word.size()+1;
 	}
-	continue;
-      } 
-      auto fail_pos=failure_function[i];
-      str.remove_prefix(i+1-fail_pos);
-      i=fail_pos-1;
+      }
     }
-    return {};
+
+    return nullptr;
   }
 } // namespace cyy::lang
