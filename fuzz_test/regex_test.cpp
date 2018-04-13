1124a271df511c2b0d2a60d3ef70eb0f93c5c855
cyy <cyyever@outlook.com>
2018-04-13 18:15:56 +0800

    add fuzz test

---
+++ fuzz_test/regex_test.cpp
@@ -0,0 +1,24 @@
+/*!
+ * \file regex_test.cpp
+ *
+ * \brief 测试正則
+ */
+#include <iostream>
+
+#include "../src/ascii.hpp"
+#include "../src/regex.hpp"
+
+using namespace cyy::lang;
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
+  symbol_string expr;
+
+  for (size_t i = 0; i < Size; i++) {
+    expr.push_back(Data[i]);
+  }
+  try {
+    regex reg("ASCII", expr);
+  } catch (const std::runtime_error &) {
+  }
+  return 0; // Non-zero return values are reserved for future use.
+}
