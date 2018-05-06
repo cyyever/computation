a92588810db238e1ad0256a2a4b6c7c5f2ebac6d
cyy <cyyever@outlook.com>
2018-05-06 11:41:45 +0800

    use SLR method instead of LL method for parsing regex grammar

--- src/regex_parse.cpp
+++ src/regex_parse.cpp
@@ -5,8 +5,10 @@
  * \date 2018-03-04
  */
 
+#include <cassert>
+
 #include "common_tokens.hpp"
-#include "ll_grammar.hpp"
+#include "slr_grammar.hpp"
 #include "regex.hpp"
 
 namespace cyy::lang {
@@ -28,26 +30,6 @@ regex::parse(symbol_string_view view) const {
      rprimary -> '\' ASCII-char
      rprimary -> 'non-operator-char'
      ASCII-char -> 'any-ASCII-char'
-
-     =>
-
-     rexpr -> rterm rexpr'
-     rexpr' -> '|' rterm rexpr'
-     rexpr' -> epsilon
-
-     rterm -> rfactor rterm'
-     rterm' -> rfactor rterm'
-     rterm' -> epsilon
-
-     rfactor -> rprimary rfactor'
-     rfactor' -> '*' rfactor'
-     rfactor' -> epsilon
-
-     rprimary -> '(' rexpr ')'
-     rprimary -> '\' ASCII-char
-     rprimary -> 'non-operator-char'
-     ASCII-char -> 'any-ASCII-char'
-
   */
 
   std::set<CFG::terminal_type> operators{'|', '*', '(', '\\', ')'};
@@ -81,69 +63,56 @@ regex::parse(symbol_string_view view) const {
     }
   });
 
-  CFG cfg("ASCII", "rexpr", productions);
-  cfg.eliminate_left_recursion({"rexpr", "rterm", "rfactor", "rprimary"});
-  LL_grammar regex_grammar(std::move(cfg));
+  SLR_grammar regex_grammar("ASCII", "rexpr", productions);
 
   auto parse_tree = regex_grammar.parse(view);
-  if (!parse_tree) {
-    throw std::runtime_error("regex grammar is not LL(1) grammar");
+  if(!parse_tree) {
+	  throw std::runtime_error("parse regex failed");
+
   }
 
   using syntax_node_ptr = std::shared_ptr<regex::syntax_node>;
 
   auto construct_syntex_tree =
-      [](auto &&self, const CFG::parse_node_ptr &root_parse_node,
-         syntax_node_ptr left_node) -> syntax_node_ptr {
+      [](auto &&self, const CFG::parse_node_ptr &root_parse_node
+	 ) -> syntax_node_ptr {
     std::shared_ptr<regex::syntax_node> root_syntax_node;
 
     if (auto ptr = std::get_if<CFG::nonterminal_type>(
             &(root_parse_node->grammar_symbol))) {
       if (*ptr == "rexpr") {
-        left_node = self(self, root_parse_node->children[0], nullptr);
-        return self(self, root_parse_node->children[1], left_node);
-      }
-      if (*ptr == "rexpr'") {
-        if (root_parse_node->children.size() ==
-            3) { //  rexpr' -> '|' rterm rexpr'
-          return self(self, root_parse_node->children[2],
+	if(root_parse_node->children.size()==1) {
+		return self(self,root_parse_node->children[0]);
+	} else {
+
+          return 
                       std::make_shared<regex::union_node>(
-                          left_node,
-                          self(self, root_parse_node->children[1], nullptr)));
-        }
-        return left_node;
-      }
-      if (*ptr == "rterm") {
-        left_node = self(self, root_parse_node->children[0], nullptr);
-        return self(self, root_parse_node->children[1], left_node);
-      }
-      if (*ptr == "rterm'") {
-        if (root_parse_node->children.size() == 2) { // rterm' -> rfactor rterm'
-          return self(self, root_parse_node->children[1],
-                      std::make_shared<regex::concat_node>(
-                          left_node,
-                          self(self, root_parse_node->children[0], nullptr)));
-        }
-        return left_node;
+                          self(self, root_parse_node->children[0]),
+                          self(self, root_parse_node->children[2])
+			  );
 
+	}
+      }
+      else if (*ptr == "rterm") {
+	if(root_parse_node->children.size()==1) {
+		return self(self,root_parse_node->children[0]);
+	} else {
+          return std::make_shared<regex::concat_node>( self(self, root_parse_node->children[0]) ,self(self, root_parse_node->children[1]));
+	}
       } else if (*ptr == "rfactor") {
-        left_node = self(self, root_parse_node->children[0], nullptr);
-        return self(self, root_parse_node->children[1], left_node);
-      } else if (*ptr == "rfactor'") {
-        if (root_parse_node->children.size() == 2) { // rfactor' -> '*' rfactor'
-          return self(self, root_parse_node->children[1],
-                      std::make_shared<regex::kleene_closure_node>(left_node));
-        }
-        return left_node;
-
+	if(root_parse_node->children.size()==1) {
+		return self(self,root_parse_node->children[0]);
+	} else {
+          return std::make_shared<regex::kleene_closure_node>( self(self, root_parse_node->children[0]));
+	}
       } else if (*ptr == "rprimary") {
         auto first_terminal = std::get<CFG::terminal_type>(
             root_parse_node->children[0]->grammar_symbol);
         switch (first_terminal) {
         case '(':
-          return self(self, root_parse_node->children[1], nullptr);
+          return self(self, root_parse_node->children[1]);
         case '\\':
-          return self(self, root_parse_node->children[1], nullptr);
+          return self(self, root_parse_node->children[1]);
         default:
           return std::make_shared<regex::basic_node>(first_terminal);
         }
@@ -153,9 +122,10 @@ regex::parse(symbol_string_view view) const {
         return std::make_shared<regex::basic_node>(first_terminal);
       }
     }
-    throw std::runtime_error("should no go here");
+    assert(0);
+    return {};
   };
 
-  return construct_syntex_tree(construct_syntex_tree, parse_tree, nullptr);
+  return construct_syntex_tree(construct_syntex_tree, parse_tree);
 }
 } // namespace cyy::lang
