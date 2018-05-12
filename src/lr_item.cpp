27794ee45ee9fbafd773979ae8890dc9441acfe5
cyy <cyyever@outlook.com>
2018-05-12 13:24:51 +0800

    fix bug of iteration

--- src/lr_item.cpp
+++ src/lr_item.cpp
@@ -10,37 +10,38 @@
 namespace cyy::computation {
 
 void LR_0_item_set ::add_kernel_item(const CFG &cfg, LR_0_item kernel_item) {
-  std::set<CFG::nonterminal_type> tmp_nonkernel_items;
+
+  std::vector<CFG::nonterminal_type> tmp_nonkernel_items;
   if (kernel_item.dot_pos < kernel_item.production.second.size()) {
     auto const &symbol = kernel_item.production.second[kernel_item.dot_pos];
     if (auto ptr = std::get_if<CFG::nonterminal_type>(&symbol); ptr) {
-      tmp_nonkernel_items.insert(*ptr);
-    }
-  }
-
-  bool has_added = true;
-  while (has_added) {
-    has_added = false;
-
-    for (const auto &nonkernel_item : tmp_nonkernel_items) {
-      auto it = cfg.get_productions().find(nonkernel_item);
-
-      for (auto const &body : it->second) {
-        if (cfg.is_epsilon(body[0])) {
-          kernel_items.emplace(LR_0_item{{it->first, body}, 1});
-          continue;
-        }
-
-        if (auto ptr = std::get_if<CFG::nonterminal_type>(&body[0]); ptr) {
-          if (tmp_nonkernel_items.insert(*ptr).second) {
-            has_added = true;
-          }
-        }
+      if(nonkernel_items.count(*ptr)==0) {
+	tmp_nonkernel_items.push_back(*ptr);
       }
     }
   }
+
   kernel_items.emplace(std::move(kernel_item));
-  nonkernel_items.merge(tmp_nonkernel_items);
+  while (!tmp_nonkernel_items.empty()) {
+    auto  nonkernel_item = std::move(tmp_nonkernel_items.back());
+    tmp_nonkernel_items.pop_back();
+    nonkernel_items.emplace(nonkernel_item);
+
+    auto it = cfg.get_productions().find(nonkernel_item);
+    for (auto const &body : it->second) {
+      if (cfg.is_epsilon(body[0])) {
+	kernel_items.emplace(LR_0_item{{it->first, body}, 1});
+	continue;
+      }
+
+      if (auto ptr = std::get_if<CFG::nonterminal_type>(&body[0]); ptr) {
+	if(nonkernel_items.count(*ptr)==0) {
+	  tmp_nonkernel_items.push_back(*ptr);
+	}
+      }
+    }
+  }
+
 }
 
 void LR_1_item_set::add_kernel_item(
