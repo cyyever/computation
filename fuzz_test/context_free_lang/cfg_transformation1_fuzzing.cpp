1bc2e71542308e7728e0efa83e89a833254d14cc
cyy <cyyever@outlook.com>
2020-02-02 10:14:42 +0800

    add fuzzing

---
+++ fuzz_test/context_free_lang/cfg_transformation1_fuzzing.cpp
@@ -0,0 +1,31 @@
+/*!
+ * \file canonical_lr_grammar_fuzzing.cpp
+ *
+ * \brief
+ * \author cyy
+ * \date 2019-02-14
+ */
+
+#include "../../src/context_free_lang/canonical_lr_grammar.hpp"
+#include "../../src/lang/common_tokens.hpp"
+#include "../helper.hpp"
+
+using namespace cyy::computation;
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
+  auto productions = fuzzing_CFG_productions(Data, Size);
+
+  CFG::nonterminal_type start_symbol;
+  if (!productions.empty()) {
+    start_symbol = productions.begin()->first;
+  }
+
+  try {
+    CFG cfg("common_tokens", start_symbol, productions);
+    cfg.eliminate_left_recursion();
+    cfg = old_cfg;
+    cfg.left_factoring();
+  } catch (const std::invalid_argument &) {
+  }
+  return 0; // Non-zero return values are reserved for future use.
+}
---
+++ fuzz_test/context_free_lang/cfg_transformation3_fuzzing.cpp
@@ -0,0 +1,29 @@
+/*!
+ * \file canonical_lr_grammar_fuzzing.cpp
+ *
+ * \brief
+ * \author cyy
+ * \date 2019-02-14
+ */
+
+#include "../../src/context_free_lang/canonical_lr_grammar.hpp"
+#include "../../src/lang/common_tokens.hpp"
+#include "../helper.hpp"
+
+using namespace cyy::computation;
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
+  auto productions = fuzzing_CFG_productions(Data, Size);
+
+  CFG::nonterminal_type start_symbol;
+  if (!productions.empty()) {
+    start_symbol = productions.begin()->first;
+  }
+
+  try {
+    CFG cfg("common_tokens", start_symbol, productions);
+    cfg.to_PDA();
+  } catch (const std::invalid_argument &) {
+  }
+  return 0; // Non-zero return values are reserved for future use.
+}
