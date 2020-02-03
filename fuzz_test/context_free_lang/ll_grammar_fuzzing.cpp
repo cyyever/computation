a887a0c581237d213ff1c4a4117e6195e93b7519
cyy <cyyever@outlook.com>
2020-02-03 23:23:50 +0800

    fix code

--- fuzz_test/context_free_lang/ll_grammar_fuzzing.cpp
+++ fuzz_test/context_free_lang/ll_grammar_fuzzing.cpp
@@ -14,15 +14,17 @@ using namespace cyy::computation;
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size);
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
-  auto productions = fuzzing_CFG_productions(Data, Size);
-
+  auto part_size = Size / 2;
+  auto productions = fuzzing_CFG_productions(Data, part_size);
   CFG::nonterminal_type start_symbol;
   if (!productions.empty()) {
     start_symbol = productions.begin()->first;
   }
 
+  auto str = fuzzing_symbol_string(Data + part_size, Size - part_size);
   try {
     LL_grammar grammar("common_tokens", start_symbol, productions);
+    static_cast<void>(grammar.parse(str, [](auto &, auto) {}));
   } catch (const std::invalid_argument &) {
   }
   return 0; // Non-zero return values are reserved for future use.
