d81917de19413ecb2c63564cdf5700dcec070b7f
cyy <cyyever@outlook.com>
2020-02-17 07:47:57 +0800

    fix fuzz

---
+++ fuzz_test/context_free_lang/cnf_fuzzing.cpp
@@ -0,0 +1,32 @@
+/*!
+ * \file canonical_lr_grammar_fuzzing.cpp
+ *
+ * \brief
+ * \author cyy
+ * \date 2019-02-14
+ */
+
+#include "../../src/context_free_lang/cnf.hpp"
+#include "../../src/lang/common_tokens.hpp"
+#include "../helper.hpp"
+
+using namespace cyy::computation;
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size);
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
+  auto part_size = Size / 2;
+  auto productions = fuzzing_CFG_productions(Data, part_size);
+
+  CFG::nonterminal_type start_symbol;
+  if (!productions.empty()) {
+    start_symbol = productions.begin()->first;
+  }
+
+  auto str = fuzzing_symbol_string(Data + part_size, Size - part_size);
+  try {
+    CNF cnf("common_tokens", start_symbol, productions);
+    static_cast<void>(cnf.parse(str));
+  } catch (const std::invalid_argument &) {
+  }
+  return 0; // Non-zero return values are reserved for future use.
+}
