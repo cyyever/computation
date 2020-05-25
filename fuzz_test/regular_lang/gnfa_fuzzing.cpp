5784674559fa903db657d052de443c910270df1d
cyy <cyyever@outlook.com>
2020-05-26 00:21:55 +0800

    fix code

--- fuzz_test/regular_lang/gnfa_fuzzing.cpp
+++ fuzz_test/regular_lang/gnfa_fuzzing.cpp
@@ -3,18 +3,17 @@
  *
  * \brief 测试正則
  */
+#include "../../src/regular_lang/gnfa.hpp"
 #include "../../src/regular_lang/regex.hpp"
 #include "../helper.hpp"
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
   using namespace cyy::computation;
   try {
-    auto part_size = Size / 2;
-    auto nfa = fuzzing_NFA(Data, part_size);
-    auto str = fuzzing_symbol_string(Data + part_size, Size - part_size);
-    nfa.simulate(str);
+    auto nfa = fuzzing_NFA(Data, Size);
     auto dfa = nfa.to_DFA();
     auto gnfa = GNFA(dfa);
+    gnfa.to_regex();
   } catch (const std::invalid_argument &) {
   }
   return 0; // Non-zero return values are reserved for future use.
