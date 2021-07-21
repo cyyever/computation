4c32f73baef9ebbb9b972c2742c838ad7f2ea734
cyy <cyyever@outlook.com>
2021-07-21 16:44:48 +0800

    fix code

--- src/context_free_lang/ll_grammar.cpp
+++ src/context_free_lang/ll_grammar.cpp
@@ -140,7 +140,7 @@ namespace cyy::computation {
                   std::make_shared<parse_node>(grammar_symbol));
             }
 
-            for (auto const &child : node->children |  std::views::reverse) {
+            for (auto const &child : node->children | std::views::reverse) {
               if (child->grammar_symbol.is_nonterminal()) {
                 stack.push_back(child);
               }
